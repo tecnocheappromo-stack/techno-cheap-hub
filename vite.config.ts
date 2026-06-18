db6b88b04c2528bb13b83d33fe27dbc32e13095a
tecnocheappromo-stack <tecnocheappromo@gmail.com>
2026-06-18 16:35:36 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "@lovable-dev/vite-tanstack-config";
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 import { nitro } from "nitro/vite";
 
 export default defineConfig({
